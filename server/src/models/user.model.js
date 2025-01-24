1e23504736ed0fadc127557b958fca47348c6a7a
NikhilDaivanapally <nikhildaivanapally@gmail.com>
2025-01-24 13:28:39 +0530

    add the logo and name

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import crypto from "crypto";
-import jwt from "jsonwebtoken";
 const userSchema = new mongoose.Schema(
   {
     userName: {
@@ -140,27 +139,6 @@ userSchema.methods.createPasswordResetToken = async function () {
   return resetToken;
 };
 
-// Generate AccessToken
-userSchema.methods.generateAccessToken = function () {
-  // return jwt.sign(data,secret,expiry)
-
-  return jwt.sign({ _id: this._id }, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
-  });
-};
-
-// Generate refreshToken
-userSchema.methods.generateRefreshToken = function () {
-  return jwt.sign(
-    {
-      _id: this._id,
-    },
-    process.env.REFRESH_TOKEN_SECRET,
-    {
-      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
-    }
-  );
-};
 
 const User = mongoose.model("User", userSchema);
 export default User;
